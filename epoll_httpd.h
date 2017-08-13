45c153c8221b645a4911a39edbe158d035abb040
sdoyuxuan <513754057@qq.com>
2017-08-14 01:47:09 +0800

    epoll_httpd.h
    单进程epoll版的httpd的头文件

---
+++ epoll_httpd.h
@@ -0,0 +1,28 @@
+#ifndef _HTTPD_H_
+#define _HTTPD_H_
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <assert.h>
+#include <sys/sendfile.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/epoll.h>
+#include <errno.h>
+
+#define BACKLOG 10
+#define SIZE 4096
+
+int get_line(int sock,char*buff,int size);
+int startup(const char*ip,int port);
+void handler_request(int sock);
+void echo_www(int sock,char*path,int len);
+void echo_err(int sock);
+#endif
